1346e4c6d5e69727f6864804ee5904d21d832099
James Smith <jgsmith@gmail.com>
2011-07-03 09:30:40 -0400

    Add tests to Data.Set, Data.Type, and Data.Property

--- test/data.js
+++ test/data.js
@@ -13,15 +13,34 @@ $(document).ready(function() {
 	test("Check set construction", function() {
 		var set, list;
 		
-		expect(5);
+		expect(13);
 		ok( MITHGrid.Data.Set !== undefined, "Set exists" );
 		ok( $.isFunction(MITHGrid.Data.Set), "Set is a function" );
+		
 		set = MITHGrid.Data.Set(['a', 'bc', 'def', 4]);
 		ok( set !== undefined, "set object is not undefined" );
 		ok( set.isSet, "set object has .isSet as true" );
+		
 		list = set.items();
 		equals( 4, list.length, ".items returns right number of values" );
-
+		equals( 4, set.size(), ".size returns the right number of values" );
+		
+		set.add('foo');
+		equals( 5, set.size(), ".add adds a value" );
+		
+		set.add('foo');
+		equals( 5, set.size(), ".add adds a value only if unique" );
+		
+		set.remove('foo');
+		equals( 4, set.size(), ".add removes a value" );
+		
+		ok( !set.contains('foo'), "confirm 'foo' is no longer in set" );
+		ok( set.contains('def'), "confirm 'def' is in set" );
+		
+		// 4 and '4' are the same in a set since we're using a JS object behind the scenes
+		// since we use sets to contains lists of item ids (implicitely strings), this is okay
+		ok( set.contains(4), "confirm 4 is in set" );
+		ok( set.contains('4'), "confirm '4' is in set" );
 	});
 
     module("Data.Type");
@@ -29,9 +48,13 @@ $(document).ready(function() {
 	test("Check type construction", function() {
 		var type;
 		
-		expect(2);
+		expect(4);
 		ok( MITHGrid.Data.Type !== undefined, "Type exists" );
 		ok( $.isFunction(MITHGrid.Data.Type), "Type is a function" );
+		
+		type = MITHGrid.Data.Type('Foo');
+		equals( "object", typeof type, "Type constructor returns an object" );
+		equals( 'Foo', type.name, "Type .name returns correct name" );
 	});
 	
 	module("Data.Property");
@@ -39,9 +62,17 @@ $(document).ready(function() {
 	test("Check property construction", function() {
 		var prop;
 		
-		expect(2);
+		expect(6);
 		ok( MITHGrid.Data.Property !== undefined, "Property exists" );
 		ok( $.isFunction(MITHGrid.Data.Property), "Property is a function" );
+		
+		prop = MITHGrid.Data.Property('foo');
+		equals( "object", typeof prop, "Property constructor returns an object" );
+		equals( "foo", prop.name, "Property .name returns correct name" );
+		equals( "text", prop.getValueType(), "Property .getValueType returns correct default type" );
+		
+		prop.valueType = "item";
+		equals( "item", prop.getValueType(), "Property .getValueType returns correct type" );
 	});
 	
 	module("Data.Source");
